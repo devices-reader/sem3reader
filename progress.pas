9edc99a67ce97567e0fc19f7bdcad476fdec9d96
devices-reader <devices-reader@yandex.ru>
2015-09-30 18:26:41 +0400

    Fix

--- progress.pas
+++ progress.pas
@@ -1,37 +1,19 @@
-{?}unit progress;
+unit progress;
 
 interface
 
-//procedure InitProgress;
 procedure ShowProgress(i: integer; j: integer);
 
 implementation
 
-uses ComCtrls, main;
-{
-procedure InitProgress(StatusBar: TStatusBar; ProgressBar: TProgressBar);
-var
-  ProgressBarStyle: integer;
-begin
-  // enable status bar 2nd Panel custom drawing
-  StatusBar.Panels[5].Style := psOwnerDraw;
+uses main;
 
-  // place the progress bar into the status bar
-  ProgressBar.Parent := StatusBar;
-
-  // remove progress bar border
-  ProgressBarStyle := GetWindowLong(ProgressBar1.Handle, GWL_EXSTYLE);
-  ProgressBarStyle := ProgressBarStyle - WS_EX_STATICEDGE;
-  SetWindowLong(ProgressBar1.Handle, GWL_EXSTYLE, ProgressBarStyle);
-end;
-}
 procedure ShowProgress(i: integer; j: integer);
 begin
-  with frmMain.prbMain do
-  begin
+  with frmMain.prbMain do begin
     Min := 0;
     Max := j;
-    Position := i + 1;
+    Position := i+1;
   end;
 end;
 
